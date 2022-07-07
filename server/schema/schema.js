78c5942c6cebfa05d0e9158fcd278d0231d47184
Anik Hossain <anik.wdev@gmail.com>
2022-07-07 14:53:20 +0600

    application is ready for use

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -98,6 +98,11 @@ const mutation = new GraphQLObjectType({
                 id: { type: new GraphQLNonNull(GraphQLID) },
             },
             resolve(_, args) {
+                Project.find({ clientID: args.id }).then((projects) => {
+                    projects.forEach((project) => {
+                        project.remove();
+                    });
+                });
                 return Client.findByIdAndRemove(args.id);
             },
         },
